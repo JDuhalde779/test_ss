a22de6838ba6dc7f2cff6f7a5516b7146a87140c
Joaquin <joaquin@gmail.com>
2023-10-03 16:44:38 -0300

    New feat. RuidoRosaCompleto

--- pre_procesamiento.py
+++ pre_procesamiento.py
@@ -1,71 +1,102 @@
 import numpy as np
 from matplotlib import pyplot as plt
-import panda as pd
-def test_ruido_rosa(nrows, ncols):
-    audio = ruidoRosa_voss(nrows, ncols)
+import pandas as pd
+import soundfile as sf
+def test_ruido_rosa(nrows,ncols):
+    audio = ruidoRosa_voss(nrows,ncols)
     plt.plot(audio)
     plt.show()
+    dominio = plot_temporal_domain(audio,100)
     
-    
+tiempo = 10  
 
-def ruidoRosa_voss(nrows, ncols=16):
+def ruidoRosa_voss(duration, fs=44100):
     """
-    Genera ruido rosa utilizando el algoritmo de Voss-McCartney(https://www.dsprelated.com/showabstract/3933.php).
-    
-    .. Nota:: si 'ruidoRosa.wav' existe, este será sobreescrito
-    
+    Genera ruido rosa utilizando el algoritmo de Voss-McCartney.
+
     Parametros
     ----------
-    t : float
-        Valor temporal en segundos, este determina la duración del ruido generado.
-    rcols: int
-        Determina el número de fuentes a aleatorias a agregar.
-    fs: int
-        Frecuencia de muestreo en Hz de la señal. Por defecto el valor es 44100 Hz.
-    
-    returns: NumPy array
+    duration : float
+        Duración en segundos del ruido rosa a generar.
+    fs : int, optional
+        Frecuencia de muestreo en Hz de la señal. Por defecto, el valor es 44100 Hz.
+
+    Returns
+    -------
+    NumPy array
         Datos de la señal generada.
-    
+
     Ejemplo
     -------
-    Generar un `.wav` desde un numpy array de 10 segundos con ruido rosa a una 
-    frecuencia de muestreo de 44100 Hz.
-    
+    Generar un archivo ".wav" de 10 segundos con ruido rosa a una frecuencia de muestreo de 44100 Hz.
+
         import numpy as np
         import soundfile as sf
-        from scipy import signal
         
         ruidoRosa_voss(10)
     """
     
+    # Calculate the number of samples based on duration and sample rate
+    num_samples = int(duration * fs)
+
+    array = np.full((num_samples, 1), np.nan)
+    array[0, 0] = np.random.random()
     
-    array = np.full((nrows, ncols), np.nan)
-    array[0, :] = np.random.random(ncols)
-    array[:, 0] = np.random.random(nrows)
-    
-    # el numero total de cambios es nrows
-    n = nrows
+    # The number of changes is equal to the number of samples
+    n = num_samples
     cols = np.random.geometric(0.5, n)
-    cols[cols >= ncols] = 0
-    rows = np.random.randint(nrows, size=n)
+    cols[cols >= 1] = 0
+    rows = np.random.randint(num_samples, size=n)
     array[rows, cols] = np.random.random(n)
     
     df = pd.DataFrame(array)
     filled = df.fillna(method='ffill', axis=0)
     total = filled.sum(axis=1)
     
-    ## Centrado de el array en 0
+    # Center the array at 0
     total = total - total.mean()
     
-    ## Normalizado
-    valor_max = max(abs(max(total)),abs(min(total)))
-    total = total / valor_max
+    # Normalize
+    max_abs_value = max(abs(max(total)), abs(min(total)))
+    total = total / max_abs_value
     
+    # Ruta completa al escritorio (Desktop) 
+    desktop_path = "/Users/Educacion/Desktop/" #Se puede guardar en la carpeta deseada, aquí se utilizó como ejemplo la carpeta escritorio
+    
+    # Nombre del archivo de salida y ruta completa
+    output_audio_file = desktop_path + "RuidoRosa.wav"
     # Agregar generación de archivo de audio .wav
-    
+    sf.write("RuidoRosa.wav", total, 44100)
+    # Guarda el nuevo array amplificado de numpy como archivo de audio   
     return total
 
+def plot_temporal_domain(signal, fs= 44100):
+    """
+    Muestra el dominio temporal de la señal.
 
+    Parametros
+    ----------
+    signal : NumPy array
+        Señal a mostrar en el dominio temporal.
+    fs : int
+        Frecuencia de muestreo en Hz de la señal.
+
+    Returns
+    -------
+    None
+    """
+    # Calculate the time values
+    time = np.arange(len(signal)) / fs
+
+    # Create a new figure and plot the signal
+    plt.figure(figsize=(10, 4))
+    plt.plot(time, signal)
+    plt.title('Dominio Temporal de la Señal')
+    plt.xlabel('Tiempo (segundos)')
+    plt.ylabel('Amplitud')
+    plt.grid(True)
+    plt.show()
+    
 if __name__ == '__main__':
-    print(test_ruido_rosa(100, 10))
+    print(test_ruido_rosa(tiempo,100))
     
