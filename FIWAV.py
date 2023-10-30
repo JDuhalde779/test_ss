88a919e73cdde60ccb1776a951846eb47e4def27
MarxosGG <marcosgarciagiorgi31@gmail.com>
2023-10-29 21:16:22 -0300

    RIsinteArreglado

--- FIWAV.py
+++ FIWAV.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.io.wavfile as wav
 
+
 def generar_filtro_inverso(input_file, output_file, fs=44100):
     # Cargar el archivo .wav del sine sweep
     fs_sine_sweep, sine_sweep = wav.read(input_file)
@@ -27,4 +28,43 @@ def generar_filtro_inverso(input_file, output_file, fs=44100):
 input_file = 'toma_n1_a-03.wav'  # Nombre del archivo .wav del sine sweep
 output_file = 'filtro_inversoDR.wav'  # Nombre del archivo de salida del filtro inverso
 
-generar_filtro_inverso(input_file, output_file)
+#generar_filtro_inverso(input_file, output_file)
+
+def generar_respuesta_al_impulso(T60_lista, frecuencias_lista, duracion, archivo_salida):
+    # Número de muestras
+    fs = 44100  # Frecuencia de muestreo (puedes ajustarla)
+    n_muestras = int(fs * duracion)
+
+    # Crear el arreglo de tiempo
+    t = np.arange(0, n_muestras) / fs
+
+    # Inicializar la respuesta al impulso
+    respuesta_impulso = np.zeros(n_muestras)
+
+    # Generar la respuesta al impulso para cada frecuencia y T60 y sumarlas
+    for T60, frecuencia in zip(T60_lista, frecuencias_lista):
+        # Cálculo de τ
+        tau = -np.log(10**(-3)) / (T60)
+
+        # Generar la respuesta al impulso para la frecuencia actual
+        respuesta_frecuencia = np.exp(-tau * t) * np.cos(2 * np.pi * frecuencia * t)
+
+        # Sumar la respuesta de esta frecuencia a la respuesta global
+        respuesta_impulso += respuesta_frecuencia
+
+    # Normalizar la respuesta al impulso
+    respuesta_impulso /= np.max(np.abs(respuesta_impulso))
+
+    # Guardar la respuesta al impulso como archivo de audio .wav
+    wav.write(archivo_salida, fs, respuesta_impulso.astype(np.float32))
+
+# Ejemplo de uso de la función
+T60_lista = [0.5, 0.8, 1.2]  # Lista de T60 para cada frecuencia
+frecuencias_lista = [500, 1000, 2000]  # Lista de frecuencias centrales
+duracion = max(T60_lista) + 2  # Duración en segundos
+nombre_archivo = 'respuesta_al_impulso.wav'  # Nombre del archivo de salida
+
+generar_respuesta_al_impulso(T60_lista, frecuencias_lista, duracion, nombre_archivo)
+
+
+
