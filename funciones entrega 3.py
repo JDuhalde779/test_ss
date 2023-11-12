590758741adf776d46e806299fb8c4110dac4f4f
MarxosGG <marcosgarciagiorgi31@gmail.com>
2023-11-12 14:16:16 -0300

    NADA

--- funciones entrega 3.py
+++ funciones entrega 3.py
@@ -73,9 +73,26 @@ def filtro_promedio_movil(input_file, output_file, L):
     # Guardar la señal filtrada en un archivo WAV de salida
     wav.write(output_file, sample_rate, filtered_signal.astype(np.int16))
 
+def acortar_wav(input_path, output_path, duracion_deseada):
+    # Cargar el archivo WAV
+    audio = AudioSegment.from_wav(input_path)
+
+    # Acortar la duración según lo deseado
+    audio_recortado = audio[:duracion_deseada * 1000]  # Duración en milisegundos
+
+    # Guardar el nuevo archivo WAV
+    audio_recortado.export(output_path, format="wav")
+
+# Especifica la ruta del archivo de entrada y salida, y la nueva duración deseada en segundos
+archivo_entrada = "respuesta_al_impulsoObtenida.wav"
+archivo_salida = "impulso_recortado.wav"
+duracion_deseada = 5  # Por ejemplo, 10 segundos
+
+
+acortar_wav(archivo_entrada, archivo_salida, duracion_deseada)
 
 # Ejemplo de uso
-input_file = "respuesta_al_impulsoObtenida.wav"
+input_file = "impulso_recortado.wav"
 output_file = "salida_filtrada.wav"
 L = 100 # Número de muestras para el promedio móvil
 filtro_promedio_movil(input_file, output_file, L)
@@ -87,6 +104,33 @@ plot_dominio_temporal(audio_log_2, fs=44100, inicio=0, duracion=1.75, umbral_amp
 plot_dominio_temporal(audio_log, fs=44100, inicio=0, duracion=1.75, umbral_amplitud= -100)
 
 
+
+def regresion_lineal(x, y):
+    # Función para realizar la regresión lineal
+    X = np.vstack((np.ones(len(x)), x)).T
+    XT = np.transpose(X)
+    XTX = np.dot(XT, X)
+    XTX_inv = np.linalg.inv(XTX)
+    XTY = np.dot(XT, y)
+    coeficientes = np.dot(XTX_inv, XTY)
+    return coeficientes
+
+# Cargar la respuesta al impulso desde el archivo .wav
+respuesta_al_impulso, tasa_muestreo = sf.read(output_file)
+
+# Crear un vector de tiempo (puedes ajustar esto según la duración de tu respuesta al impulso)
+tiempo = np.arange(0, len(respuesta_al_impulso)/tasa_muestreo, 1/tasa_muestreo)
+
+# Aplicar regresión lineal
+coeficientes = regresion_lineal(tiempo, respuesta_al_impulso)
+
+# Coeficientes resultantes: coeficientes[0] es b (ordenada al origen) y coeficientes[1] es a (pendiente)
+a, b = coeficientes[1], coeficientes[0]
+
+print(f"La recta de regresión es: y = {a}x + {b}")   
+
+
+
 def calcular_edt_para_todas_las_frecuencias(ruta_archivo, frecuencias_tercios_octava):
     # Función para calcular el EDT para todas las frecuencias de tercios de octava de un archivo WAV
     tasa_muestreo, señal = wav.read(ruta_archivo)
@@ -96,6 +140,7 @@ def calcular_edt_para_todas_las_frecuencias(ruta_archivo, frecuencias_tercios_oc
         # Encontrar el índice de la frecuencia más cercana
         indice_frecuencia_cercana = int(frecuencia / (tasa_muestreo / len(señal)))
 
+        # Encontrar el pico en la región de la frecuencia de interés
         pico_indice = np.argmax(señal[indice_frecuencia_cercana:])
 
         # Encontrar el punto donde la respuesta al impulso suavizada decae 10 dB desde el pico
@@ -103,16 +148,24 @@ def calcular_edt_para_todas_las_frecuencias(ruta_archivo, frecuencias_tercios_oc
         indices_descenso, _ = find_peaks(-señal[pico_indice:], height=umbral_dB)
 
         # Calcular el EDT en segundos
-        edt_indice = pico_indice + indices_descenso[0]
-        tiempo_edt = edt_indice / tasa_muestreo
-
-        resultados_edt.append(tiempo_edt)
+        if indices_descenso.size > 0:
+            edt_indice = pico_indice + indices_descenso[0]
+            tiempo_edt = edt_indice / tasa_muestreo
+            resultados_edt.append(tiempo_edt)
+        else:
+            resultados_edt.append(None)  # Manejo de casos donde no se encuentra el punto de descenso
 
     return resultados_edt
 
 # Ejemplo de uso
-frecuencias_tercios_octava = [125, 250, 500, 1000, 2000, 4000, 8000]  # Ejemplo de frecuencias de tercios de octava
+frecuencias_tercios_octava = [125, 250, 500, 1000, 2000, 4000, 8000]
 
 resultados_edt = calcular_edt_para_todas_las_frecuencias(output_file, frecuencias_tercios_octava)
 for frecuencia, edt_resultado in zip(frecuencias_tercios_octava, resultados_edt):
-    print(f"EDT para {frecuencia} Hz: {edt_resultado} segundos")
+    if edt_resultado is not None:
+        print(f"EDT para {frecuencia} Hz: {edt_resultado} segundos")
+    else:
+        print(f"No se pudo calcular el EDT para {frecuencia} Hz")
+
+
+
--- test.py
+++ test.py
@@ -10,31 +10,132 @@ import scipy.io.wavfile as wav
 
 
 
-def calcular_edt(ruta_archivo):
+def iec61260_filtros(audio_signal, center_frequency, sample_rate=44100):
     """
-    Calcula el Early Decay Time (EDT) de un archivo WAV.
+   Aplica filtros acústicos según la norma IEC 61260 a una señal de audio.
 
-    Parámetros:
-    - ruta_archivo: Ruta del archivo WAV.
+   Esta función toma una señal de audio, la frecuencia central deseada, y opcionalmente la frecuencia de muestreo,
+   y aplica un filtro acústico de octava o tercio de octava según la norma IEC 61260.
+   La función guarda la señal filtrada en un archivo WAV individual con un nombre apropiado para la frecuencia central.
 
-    Devuelve el EDT en segundos.
-    """
-    tasa_muestreo = 44100
+   Parámetros:
+   audio_signal (array): La señal de audio de entrada.
+   center_frequency (float): La frecuencia central a la cual se aplicará el filtro.
+   sample_rate (int, opcional): La frecuencia de muestreo de la señal de audio. Valor predeterminado: 44100.
 
-    # Encontrar el pico de la respuesta al impulso
-    pico_indice = np.argmax(ruta_archivo)
+   La función guarda la señal filtrada en un archivo WAV individual con un nombre apropiado para la frecuencia central.
+   
+   Args:
+       audio_signal (array): La señal de audio de entrada.
+       center_frequency (float): La frecuencia central a la cual se aplicará el filtro.
+       sample_rate (int, opcional): La frecuencia de muestreo de la señal de audio.
 
-    # Encontrar el punto donde la respuesta al impulso decae 10 dB desde el pico
-    umbral_dB = -10
-    indices_descenso, _ = find_peaks(-ruta_archivo[pico_indice:], height=umbral_dB)
+   Returns:
+       None
+   """
+    # Lista de frecuencias centrales según la norma IEC61260 para octavas y tercios de octava
+    center_frequencies = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000]
+    frecuencias_centrales_tercio = [25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000]
 
-    # Calcular el EDT en segundos
-    edt_indice = pico_indice + indices_descenso[0]
-    tiempo_edt = edt_indice / tasa_muestreo
+    if center_frequency in center_frequencies:
+        G = 1.0 / 2.0  # Octava - G = 1.0/2.0 / 1/3 de Octava - G=1.0/6.0
+        factor = np.power(2, G)
+        center_frequency_hz = center_frequency
 
-    return tiempo_edt
+        lower_cutoff_frequency_hz = center_frequency_hz / factor
+        upper_cutoff_frequency_hz = center_frequency_hz * factor
+
+        # Para aplicar el filtro de manera más óptima
+        sos = signal.iirfilter(4, [lower_cutoff_frequency_hz, upper_cutoff_frequency_hz],
+                               rs=60, btype='band', analog=False,
+                               ftype='butter', fs=sample_rate, output='sos')
+        filtered_signal = signal.sosfilt(sos, audio_signal)
+
+        # Guarda la señal filtrada en un archivo individual
+        sf.write(f"señal_filtrada_{center_frequency}.wav", filtered_signal, sample_rate)
+    
+    if center_frequency in frecuencias_centrales_tercio:
+        G = 1.0 / 6.0  # Octava - G = 1.0/2.0 / 1/3 de Octava - G=1.0/6.0
+        factor = np.power(2, G)
+        center_frequency_hz = center_frequency
+
+        lower_cutoff_frequency_hz = center_frequency_hz / factor
+        upper_cutoff_frequency_hz = center_frequency_hz * factor
+
+        # Para aplicar el filtro de manera más óptima
+        sos = signal.iirfilter(4, [lower_cutoff_frequency_hz, upper_cutoff_frequency_hz],
+                               rs=60, btype='band', analog=False,
+                               ftype='butter', fs=sample_rate, output='sos')
+        filtered_signal = signal.sosfilt(sos, audio_signal)
+
+        # Guarda la señal filtrada en un archivo individual
+        sf.write(f"señal_filtrada_tercio_{center_frequency}.wav", filtered_signal, sample_rate)
+    
+    else:
+        print("Se ha ingresado un valor de frecuencia inválido")
+
+# Llamar a la función con alguna RI generada anteriormente.
+audio_signal, sample_rate = sf.read("respuesta_al_impulsoObtenida.wav")
+iec61260_filtros(audio_signal,1000, sample_rate=44100)
+
+
+def regresion_lineal(x, y):
+    # Función para realizar la regresión lineal
+    X = np.vstack((np.ones(len(x)), x)).T
+    XT = np.transpose(X)
+    XTX = np.dot(XT, X)
+    XTX_inv = np.linalg.inv(XTX)
+    XTY = np.dot(XT, y)
+    coeficientes = np.dot(XTX_inv, XTY)
+    return coeficientes
+
+# Cargar la respuesta al impulso desde el archivo .wav
+respuesta_al_impulso, tasa_muestreo = sf.read("salida_filtrada.wav")
+
+# Crear un vector de tiempo (puedes ajustar esto según la duración de tu respuesta al impulso)
+tiempo = np.arange(0, len(respuesta_al_impulso)/tasa_muestreo, 1/tasa_muestreo)
+
+# Aplicar regresión lineal
+coeficientes = regresion_lineal(tiempo, respuesta_al_impulso)
+
+# Coeficientes resultantes: coeficientes[0] es b (ordenada al origen) y coeficientes[1] es a (pendiente)
+a, b = coeficientes[1], coeficientes[0]
+
+print(f"La recta de regresión es: y = {a}x + {b}")   
+
+
+def calcular_edt_con_regresion(ruta_archivo, umbral_dB=10, ventana_suavizado=100):
+    # Función para calcular el EDT utilizando la regresión lineal para determinar el tiempo donde la respuesta cae 10 dB desde el pico
+    tasa_muestreo, señal = wav.read(ruta_archivo)
+
+
+    # Encontrar el índice del pico en la región de interés
+    pico_indice = np.argmax(señal)
+
+    # Encontrar el punto donde la respuesta al impulso suavizada decae 10 dB desde el pico
+    umbral_amplitud = np.max(señal) - umbral_dB
+    indices_descenso, _ = find_peaks(-señal[pico_indice:], height=umbral_amplitud)
+
+    # Calcular el tiempo de EDT utilizando la regresión lineal
+    if indices_descenso.size > 0:
+        tiempo_edt = pico_indice + indices_descenso[0]
+        tiempo_edt /= tasa_muestreo
+
+        # Calcular la regresión lineal para la parte posterior al pico
+        x_post_pico = np.arange(pico_indice, len(señal)) / tasa_muestreo
+        y_post_pico = señal[pico_indice:]
+
+        # Calcular el tiempo de decaimiento usando la regresión lineal
+        tiempo_decaimiento_regresion = -umbral_dB / coeficientes[1]
+        tiempo_decaimiento_regresion += tiempo_edt
+        print(f"Tiempo de decaimiento (regresión): {tiempo_decaimiento_regresion} segundos")
+    else:
+        print("No se pudo calcular el tiempo de decaimiento utilizando la regresión lineal.")
 
 # Ejemplo de uso
-ruta_archivo_wav = "ejemplo.wav"  # Reemplaza con la ruta de tu archivo WAV
-edt_resultado = calcular_edt(ruta_archivo_wav)
-print(f"EDT: {edt_resultado} segundos")
+output_file = "salida_filtrada.wav"  # Reemplazar con la ruta correcta de tu archivo WAV
+calcular_edt_con_regresion(output_file)
+
+
+
+
